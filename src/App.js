3ada5f728ac25c4cc7364be5797e6d8cea5428fa
wnstjr <wnstjr8717@gmail.com>
2023-04-26 14:42:41 +0900

    login axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./gradientAnimation.css";
-import { Routes, Route } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import TermsPage from "./pages/TermsPage";
 import Privacy from "./pages/Privacy";
@@ -10,50 +10,55 @@ import ServiceCompany from "./pages/ServiceCompany";
 import ServiceDeveloper from "./pages/ServiceDeveloper";
 import Pricing from "./pages/Pricing";
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import {initReactI18next} from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import { ko, en } from "./locales";
+import {en, ko} from "./locales";
 import XHR from "i18next-xhr-backend";
 import Browse from "./pages/Browse";
 import DeveloperProfile from "./pages/DeveloperProfile";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import axios, {AxiosInterceptor} from "./utils/authAxios";
+import {useEffect, useState} from "react";
 
 function App() {
-  const options = {
-    order: ["querystring", "navigator"],
-    lookupQuerystring: "lng",
-  };
-  i18n
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .use(XHR)
-    .init({
-      resources: { en, ko },
-      defaultNS: "common",
-      supportedLngs: ["en", "ko"],
-      ns: ["common"],
-      fallbackLng: "en",
-      interpolation: { escapeValue: false },
-      detection: options,
-    });
+    const [accessToken, setAccessToken] = useState(null)
+    const options = {
+        order: ["querystring", "navigator"],
+        lookupQuerystring: "lng",
+    };
+    i18n
+        .use(LanguageDetector)
+        .use(initReactI18next)
+        .use(XHR)
+        .init({
+            resources: {en, ko},
+            defaultNS: "common",
+            supportedLngs: ["en", "ko"],
+            ns: ["common"],
+            fallbackLng: "en",
+            interpolation: {escapeValue: false},
+            detection: options,
+        });
 
-  return (
-    <Routes>
-      <Route path="/*" element={<MainPage />} />
-      <Route path="/browse" element={<Browse />} />
-      <Route path="/developer/:userId" element={<DeveloperProfile />} />
-      <Route path="/service/company" element={<ServiceCompany />} />
-      <Route path="/service/developer" element={<ServiceDeveloper />} />
-      <Route path="/pricing" element={<Pricing />} />
-      <Route path="legal/" element={<Legal />} />
-      <Route path="legal/terms-of-use" element={<TermsPage />} />
-      <Route path="legal/privacy-policy" element={<Privacy />} />
-      <Route path="legal/payment-terms" element={<PaymentTerms />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-  );
+    return (
+        <AxiosInterceptor setAccessToken={setAccessToken}>
+            <Routes>
+                <Route path="/*" element={<MainPage/>}/>
+                <Route path="/browse" element={<Browse/>}/>
+                <Route path="/developer/:userId" element={<DeveloperProfile/>}/>
+                <Route path="/service/company" element={<ServiceCompany/>}/>
+                <Route path="/service/developer" element={<ServiceDeveloper/>}/>
+                <Route path="/pricing" element={<Pricing/>}/>
+                <Route path="legal/" element={<Legal/>}/>
+                <Route path="legal/terms-of-use" element={<TermsPage/>}/>
+                <Route path="legal/privacy-policy" element={<Privacy/>}/>
+                <Route path="legal/payment-terms" element={<PaymentTerms/>}/>
+                <Route path="/login" element={<Login accessToken={accessToken} setAccessToken={setAccessToken}/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+            </Routes>
+        </AxiosInterceptor>
+    );
 }
 
 export default App;
---
+++ src/utils/authAxios.js
@@ -0,0 +1,37 @@
+import Axios, {HttpStatusCode} from "axios";
+import {apiUrl} from "./config";
+import {useEffect} from "react";
+
+const axios = Axios.create({
+    baseURL: apiUrl
+});
+
+axios.defaults.withCredentials = true;
+
+function AxiosInterceptor({children, setAccessToken}) {
+    useEffect(() => {
+        const resInterceptor = (response) => {
+            return response;
+        };
+        const errInterceptor = (error) => {
+            if (error.response.status === HttpStatusCode.Unauthorized && error.config.url !== '/v1/auth/refresh') {
+                return axios.post(`/v1/auth/refresh`).then((response) => {
+                    if (response.status === HttpStatusCode.Ok) {
+                        setAccessToken(response.data.access_token)
+                        //TODO Retry prev API call (error)
+                    }
+                })
+            } else if (error.response.status === HttpStatusCode.Unauthorized) return error
+            return Promise.reject(error);
+        };
+        const interceptor = axios.interceptors.response.use(
+            resInterceptor,
+            errInterceptor
+        );
+        return () => axios.interceptors.response.eject(interceptor);
+    }, [setAccessToken]);
+    return children
+}
+
+export default axios;
+export {AxiosInterceptor};
