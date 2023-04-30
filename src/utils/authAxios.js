e44cd537a000c1c2f1844adf5bb8f96591c852d1
wnstjr <wnstjr8717@gmail.com>
2023-04-30 21:39:46 +0900

    invalid refresh return 400

--- src/utils/authAxios.js
+++ src/utils/authAxios.js
@@ -14,14 +14,14 @@ function AxiosInterceptor({children, setAccessToken}) {
             return response;
         };
         const errInterceptor = (error) => {
-            if (error.response.status === HttpStatusCode.Unauthorized && error.config.url !== '/v1/auth/refresh') {
+            if (error.response.status === HttpStatusCode.BadRequest && error.config.url !== '/v1/auth/refresh') {
                 return axios.post(`/v1/auth/refresh`).then((response) => {
                     if (response.status === HttpStatusCode.Ok) {
                         setAccessToken(response.data.access_token)
                         //TODO Retry prev API call (error)
                     }
                 })
-            } else if (error.response.status === HttpStatusCode.Unauthorized) return error
+            } else if (error.response.status === HttpStatusCode.BadRequest) return error
             return Promise.reject(error);
         };
         const interceptor = axios.interceptors.response.use(
