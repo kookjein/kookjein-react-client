13ff9bfd6dede7988bf51bb68faed6cd49e4c501
Andrew Jang <dhj9817@gmail.com>
2023-08-11 20:27:04 +0900

    .

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 const History = () => {
-  const Cell = ({status}) => (
+  const Cell = ({ date, status, method, badge }) => (
     <div className="h-14 w-full flex space-x-2 tracking-tight text-sm">
-      <div className="w-48 flex-shrink-0 flex items-center ml-8 space-x-2 group cursor-pointer">
+      <div className="w-24 flex-shrink-0 flex items-center ml-8 text-xs">{date}</div>
+      <div className="w-48 flex-shrink-0 flex items-center space-x-2 group cursor-pointer">
         <p className="group-hover:underline group-hover:text-green-700">장동해</p>
       </div>
       <div className="w-full flex items-center turncate space-x-2 group cursor-pointer">
-        <div className="px-1 py-0.5 bg-purple-100 text-purple-500 font-bold text-xs rounded">마일스톤 1</div>
+        {badge && <div className="px-1 py-0.5 bg-purple-100 text-purple-500 font-bold text-xs rounded">{badge}</div>}
         <p className="group-hover:underline group-hover:text-green-700">커머스 서비스 플랫폼 개발</p>
       </div>
-      <div className="w-32 flex-shrink-0 flex items-center">5월 12일</div>
-      <div className="w-32 flex-shrink-0 flex items-center">4월 12일</div>
+      <div className="w-40 flex-shrink-0 flex items-center">{method}</div>
       <div className="w-32 flex-shrink-0 flex items-center">₩3,300,000</div>
-      <div className="w-36 flex-shrink-0 flex items-center">{status}</div>
+      <div className="w-36 flex-shrink-0 flex items-center pr-4">{status}</div>
     </div>
   );
 
@@ -25,19 +25,27 @@ const History = () => {
       >
         <p className="text-2xl font-bold text-gray-800 my-6">결제 내역</p>
 
-        <div className="w-full border rounded-xl bg-white shadow-lg overflow-hidden">
-          <div className="w-full h-12 border-b flex text-xs font-bold text-gray-500 space-x-2">
-            <p className="w-48 flex-shrink-0 flex items-center ml-8">개발자 / 클라이언트</p>
+        <div className="w-full border rounded-lg bg-white shadow-lg overflow-hidden">
+          <div className="w-full h-10 bg-gray-200 border-b flex text-xs font-bold text-gray-600 space-x-2">
+            <p className="w-24 flex-shrink-0 flex items-center ml-8">일시</p>
+            <p className="w-48 flex-shrink-0 flex items-center">개발자 / 클라이언트</p>
             <p className="w-full flex items-center">프로젝트 제목</p>
-            <p className="w-32 flex-shrink-0 flex items-center">데드라인</p>
-            <p className="w-32 flex-shrink-0 flex items-center">결제일</p>
+            <p className="w-40 flex-shrink-0 flex items-center">프로젝트 방식</p>
             <p className="w-32 flex-shrink-0 flex items-center">결제 금액</p>
-            <p className="w-36 flex-shrink-0 flex items-center">상태</p>
+            <p className="w-36 flex-shrink-0 flex items-center pr-4">상태</p>
           </div>
           <div className="divide-y">
-            <Cell status={"에스크로 결제완료"} />
-            <Cell status={"개발자 지급완료"} />
-            <Cell status={"결제 취소"} />
+            <Cell date="5월 14일" badge={"마일스톤 2"} status={"에스크로 결제완료"} method="마일스톤 단위 결제" />
+            <Cell date="5월 13일" badge={"마일스톤 1"} status={"개발자 지급완료"} method="마일스톤 단위 결제" />
+            <Cell date="4월 14일" badge={"마일스톤 1"} status={"에스크로 결제완료"} method="마일스톤 단위 결제" />
+            <Cell date="2월 11일" status={"결제 취소"} method="프로젝트 전체 결제" />
+            <Cell date="2월 11일" status={"에스크로 결제완료"} method="프로젝트 전체 결제" />
+
+            <Cell date="22' 5월 14일" badge={"5월 월급"} status={"에스크로 결제완료"} method="월급" />
+            <Cell date="22' 5월 14일" badge={"4월 월급"} status={"개발자 지급완료"} method="월급" />
+            <Cell date="22' 4월 14일" badge={"4월 월급"} status={"에스크로 결제완료"} method="월급" />
+            <Cell date="22' 4월 14일" badge={"3월 월급"} status={"개발자 지급완료"} method="월급" />
+            <Cell date="22' 3월 14일" badge={"3월 월급"} status={"에스크로 결제완료"} method="월급" />
           </div>
         </div>
       </div>
