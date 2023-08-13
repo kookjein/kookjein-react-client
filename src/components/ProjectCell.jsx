4b1ed371ea69703c0fbfc4b92e2a884f99570824
wnstjr <wnstjr8717@gmail.com>
2023-08-14 01:40:01 +0900

    developer page dev profile, get all projects

--- src/components/ProjectCell.jsx
+++ src/components/ProjectCell.jsx
@@ -1,51 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
-const ProjectCell = ({ border, small }) => {
-  const Tags = ({ title }) => (
-    <div className="text-xs px-3 py-1 rounded-full bg-green-800 bg-opacity-10 text-green-800 hover:bg-opacity-20 cursor-pointer flex-shrink-0">
-      {title}
-    </div>
-  );
-  return (
-    <Link to={`/jobs/1`}>
-      <button
-        className={`${border ? "border" : "border-b"} p-8 w-full bg-white group hover:bg-green-600 hover:bg-opacity-5`}
-      >
-        <p className="text-lg font-bold group-hover:text-green-700 cursor-pointer text-gray-700 w-full text-left">
-          커머스 서비스 플랫폼 개발
-        </p>
+const ProjectCell = ({border, small, project}) => {
+    const {t, i18n} = useTranslation("profile");
+    const lang = i18n.language.includes("en") ? "en" : "ko";
+    console.log(project);
+    const Tags = ({title}) => (<div
+        className="text-xs px-3 py-1 rounded-full bg-green-800 bg-opacity-10 text-green-800 hover:bg-opacity-20 cursor-pointer flex-shrink-0">
+        {title}</div>);
+    return (<Link to={`/jobs/1`}>
+        <button
+            className={`${border ? "border" : "border-b"} p-8 w-full bg-white group hover:bg-green-600 hover:bg-opacity-5`}>
+            <p className="text-lg font-bold group-hover:text-green-700 cursor-pointer text-gray-700 w-full text-left">
+                {project.project_info[0].title[lang]}
+            </p>
 
-        <div className="flex space-x-3 mt-6 text-gray-600">
-          <div className="text-xs font-bold">예상비용 4,000 만원.</div>
-          <div className="text-xs">단기 프로젝트.</div>
-          <div className="text-xs">예상기간 120일.</div>
-          <div className="text-xs">마감일정 D-4.</div>
-          <div className="text-xs">- 등록시간: 1시간전</div>
-        </div>
+            <div className="flex space-x-3 mt-6 text-gray-600">
+                <div className="text-xs font-bold">{project.project_info[0].budget.label}</div>
+                <div className="text-xs">{project.project_info[0].method === 'contract' ? '단기 프로젝트' : '인력 구인'}</div>
+                <div className="text-xs">예상기간 {project.project_info[0].duration}</div>
+                <div className="text-xs">마감일정 {project.project_info[0].start_at}</div>
+                <div className="text-xs">- 등록시간: {project.project_created_at}</div>
+            </div>
 
-        <div className="text-xs mt-4 line-clamp-4 tracking-tight w-full text-left break-keep">
-          ※ 프로젝트의 진행 방식 - 최초 온라인 인터뷰 - 계약 방식 : 기간제 계약 - 근무 형태 : 주 5회 풀상주 - 근무지 :
-          서울특별시, 영등포구 - 근무기간 : 6개월 ~ 1년 :: 협의 가능 - 근무시간 : 오전 9시 ~ 오후 6시 - 희망 근무 시작일
-          : 8월 1째주 이내 (ASAP) - 개인장비 지참가능합니다. 필요 시 지원가능합니다. - 연차 협의 가능합니다. + 필요인력
-          및 월급여 - front-end | 중고급(6년 차 이 개인장비 지참가능합니다. 필요 시 지원가능합니다. - 연차 협의
-          가능합니다. + 필요인력 및 월급여 - front-end 개인장비 지참가능합니다. 필요 시 지원가능합니다. - 연차 협의
-          가능합니다. + 필요인력 및 월급여 - front-end | 중고급(6년 차 이 개인장비 지참가능합니다. 필요 시
-          지원가능합니다. - 연차 협의 가능합니다. + 필요인력 및 월급여 - front-end
-        </div>
+            <div className="text-xs mt-4 line-clamp-4 tracking-tight w-full text-left break-keep">
+                {project.project_info[0].detail}
+            </div>
 
-        <div className="text-xs w-full text-left mt-6 font-bold text-gray-700">지원자: 0</div>
+            <div className="text-xs w-full text-left mt-6 font-bold text-gray-700">지원자: 0</div>
 
-        <div className="flex gap-2 mt-6 flex-wrap">
-          <Tags title="React.js" />
-          <Tags title="Javascript" />
-          <Tags title="front-end" />
-          <Tags title="backend" />
-          <Tags title="aws" />
-        </div>
-      </button>
-    </Link>
-  );
+            <div className="flex gap-2 mt-6 flex-wrap">
+                {project.project_info[0].tech.map(value => (<Tags key={value.id} title={value.text}/>))}
+            </div>
+        </button>
+    </Link>);
 };
 
 export default ProjectCell;
--- src/pages/MainDeveloper.jsx
+++ src/pages/MainDeveloper.jsx
@@ -1,174 +1,159 @@
-import React, { useEffect } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import ProjectCell from "../components/ProjectCell";
 import EmptyFile from "../assets/empty-file.png";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import axios from "../utils/authAxios";
-import { useState } from "react";
-import { AuthContext } from "../context/authContext";
-import { useContext } from "react";
+import {AuthContext} from "../context/authContext";
 import DefaultImage from "../assets/default-profile.png";
 import JobFilter from "../components/JobFilter";
-import { BiLinkExternal } from "react-icons/bi";
+import {BiLinkExternal} from "react-icons/bi";
+import {useTranslation} from "react-i18next";
 
 const MainDeveloper = () => {
-  const { userState } = useContext(AuthContext);
-  const [, setMyInfo] = useState();
-  const [selectedTab, setSelectedTab] = useState("진행중");
+    const {userState} = useContext(AuthContext);
+    const [myInfo, setMyInfo] = useState();
+    const [projects, setProjects] = useState();
+    const [selectedTab, setSelectedTab] = useState("프로젝트 찾기");
+    const {t, i18n} = useTranslation("profile");
+    const lang = i18n.language.includes("en") ? "en" : "ko";
 
-  useEffect(() => {
-    axios
-      .get(`/v1/user/`, { params: { user_id: userState.user.userId } })
-      .then((response) => {
-        console.log(response.data);
-        setMyInfo(response.data);
-      })
-      .catch((e) => {
-        console.log("V1/USER/ ERROR : ", e);
-      });
-  }, [userState]);
+    useEffect(() => {
+        axios
+            .get(`/v1/user/`, {params: {user_id: userState.user.userId}})
+            .then((response) => {
+                console.log(response.data);
+                setMyInfo(response.data);
+            })
+            .catch((e) => {
+                console.log("V1/USER/ ERROR : ", e);
+            });
+        axios
+            .get(`/v1/project/all/`)
+            .then((response) => {
+                setProjects(response.data);
+            })
+    }, [userState]);
 
-  const Dashboard = () => {
-    const TabButton = ({ title }) => (
-      <button
-        onClick={() => setSelectedTab(title)}
-        className={`${
-          selectedTab === title ? "text-black" : "text-gray-400"
-        } px-6 flex items-center justify-center h-full relative hover:bg-gray-100 transition`}
-      >
-        <p className="text-sm font-bold">{title}</p>
-        {selectedTab === title && <div className="h-0.5 rounded-full w-full bg-green-600 absolute bottom-0" />}
-      </button>
-    );
+    const Dashboard = () => {
+        const TabButton = ({title}) => (<button
+                onClick={() => setSelectedTab(title)}
+                className={`${selectedTab === title ? "text-black" : "text-gray-400"} px-6 flex items-center justify-center h-full relative hover:bg-gray-100 transition`}
+            >
+                <p className="text-sm font-bold">{title}</p>
+                {selectedTab === title && <div className="h-0.5 rounded-full w-full bg-green-600 absolute bottom-0"/>}
+            </button>);
 
-    const Empty = () => (
-      <div className="flex flex-col flex-shrink-0 items-center justify-center space-y-6 py-16">
-        <img src={EmptyFile} alt="" className="w-32" />
-        <div className="flex flex-col items-center space-y-1">
-          <p className="text-lg font-bold">진행중인 프로젝트가 없습니다</p>
-          <p className="text-sm">프로젝트를 등록하면 여러 개발자들이 지원할 수 있습니다.</p>
-        </div>
+        const Empty = () => (<div className="flex flex-col flex-shrink-0 items-center justify-center space-y-6 py-16">
+                <img src={EmptyFile} alt="" className="w-32"/>
+                <div className="flex flex-col items-center space-y-1">
+                    <p className="text-lg font-bold">진행중인 프로젝트가 없습니다</p>
+                    <p className="text-sm">프로젝트를 등록하면 여러 개발자들이 지원할 수 있습니다.</p>
+                </div>
 
-        <button
-          onClick={() => setSelectedTab("프로젝트 찾기")}
-          className="h-9 px-6 bg-green-700 text-white rounded hover:brightness-125 text-sm"
-        >
-          프로젝트 찾기
-        </button>
-      </div>
-    );
-    return (
-      <div className="flex space-x-6">
-        <div className="w-full border rounded-xl py-6 bg-white shadow-lg">
-          <div className="w-full items-center border-b">
-            <h1 className="text-xl font-bold mx-8 mb-3 text-gray-700">대시보드</h1>
+                <button
+                    onClick={() => setSelectedTab("프로젝트 찾기")}
+                    className="h-9 px-6 bg-green-700 text-white rounded hover:brightness-125 text-sm"
+                >
+                    프로젝트 찾기
+                </button>
+            </div>);
+        return (<div className="flex space-x-6">
+                <div className="w-full border rounded-xl py-6 bg-white shadow-lg">
+                    <div className="w-full items-center border-b">
+                        <h1 className="text-xl font-bold mx-8 mb-3 text-gray-700">대시보드</h1>
 
-            <div className="w-full h-12 space-x-3 px-6 flex items-center">
-              <TabButton title={"진행중"} />
-              <TabButton title={"지원 현황"} />
-              <TabButton title={"프로젝트 찾기"} />
+                        <div className="w-full h-12 space-x-3 px-6 flex items-center">
+                            <TabButton title={"프로젝트 찾기"}/>
+                            <TabButton title={"진행중"}/>
+                            <TabButton title={"지원 현황"}/>
+                        </div>
+                    </div>
+                    <div className="">
+                        {selectedTab === "진행중" ? (<></>) : selectedTab === "지원 현황" ? (<>
+                                <ProjectCell/>
+                                <ProjectCell/>
+                            </>) : (<>
+                                {projects?.map((project) => <ProjectCell key={project.project_id} project={project}/>)}
+                            </>)}
+                    </div>
+
+                    <Empty/>
+                </div>
+            </div>);
+    };
+
+    const DeveloperHelp = () => (<div
+            className="mt-4 w-full border rounded-xl flex-shrink-0 flex flex-col p-6 bg-white shadow-lg space-y-4 text-gray-600">
+            <p className="text-xs">
+                Make sure to join Kookjein's Discord server when you apply for projects to get full support from our
+                team.
+            </p>
+            <a href="/">
+                <div className="flex space-x-2 items-center hover:text-blue-500">
+                    <p className="text-sm font-bold underline">Kookjein's Discord Server</p>
+                    <BiLinkExternal/>
+                </div>
+            </a>
+            <a href="/">
+                <div className="flex space-x-2 items-center hover:text-blue-500">
+                    <p className="text-sm font-bold underline">Download DEV Manual</p>
+                    <BiLinkExternal/>
+                </div>
+            </a>
+            <a href="/">
+                <div className="flex space-x-2 items-center hover:text-blue-500">
+                    <p className="text-sm font-bold underline">Help Center</p>
+                    <BiLinkExternal/>
+                </div>
+            </a>
+        </div>);
+
+    const MyProfileSummary = () => (
+        <div className="mt-4 w-full border rounded-xl flex-shrink-0 flex flex-col items-center p-6 bg-white shadow-lg">
+            <Link to={`/user/${userState.user.userId}`}>
+                <div className="flex flex-col items-center group">
+                    <img
+                        onError={({currentTarget}) => {
+                            currentTarget.onerror = null; // prevents looping
+                            currentTarget.src = DefaultImage;
+                        }}
+                        src={DefaultImage}
+                        alt=""
+                        draggable={false}
+                        className="w-16 h-16 bg-gray-100 rounded-full mb-4"
+                    />
+                    <p className="text-lg group-hover:text-green-600 group-hover:underline">{myInfo?.user.user_profile[0].name?.[lang]}</p>
+                    <p className="text-xs line-clamp-1 text-gray-600">{myInfo?.user.user_profile[0].oneLiner?.[lang]}</p>
+                </div>
+                <div className="w-full mt-6 text-sm text-green-700 font-bold">
+                    <p>프로필을 완료하세요</p>
+                    <div className="flex items-center space-x-3">
+                        <div className="w-full h-1 rounded-full bg-gray-300">
+                            <div className="w-1/2 h-full bg-green-700"></div>
+                        </div>
+                        <p className="text-xs font-normal text-gray-600">50%</p>
+                    </div>
+                </div>
+            </Link>
+        </div>);
+
+    return (<div className="w-full h-full flex flex-col items-center overflow-x-hidden bg-gray-100">
+            <div
+                style={{maxWidth: "1280px", scrollbarWidth: 0}}
+                className="w-screen sm:w-full h-full flex-shrink-0 sm:justify-around px-4 pb-24"
+            >
+                <div className="w-full mt-8 flex">
+                    <div className="w-full px-6 pb-12 space-y-4  py-4">
+                        <Dashboard/>
+                    </div>
+                    <div className="w-64 space-y-4 flex-shrink-0">
+                        <MyProfileSummary/>
+                        <DeveloperHelp/>
+                        {selectedTab === "프로젝트 찾기" && <JobFilter/>}
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="">
-            {selectedTab === "진행중" ? (
-              <></>
-            ) : selectedTab === "지원 현황" ? (
-              <>
-                <ProjectCell />
-                <ProjectCell />
-              </>
-            ) : (
-              <>
-                <ProjectCell />
-                <ProjectCell />
-                <ProjectCell />
-                <ProjectCell />
-                <ProjectCell />
-                <ProjectCell />
-              </>
-            )}
-          </div>
-
-          <Empty />
-        </div>
-      </div>
-    );
-  };
-
-  const DeveloperHelp = () => (
-    <div className="mt-4 w-full border rounded-xl flex-shrink-0 flex flex-col p-6 bg-white shadow-lg space-y-4 text-gray-600">
-      <p className="text-xs">
-        Make sure to join Kookjein's Discord server when you apply for projects to get full support from our team.
-      </p>
-      <a href="/">
-        <div className="flex space-x-2 items-center hover:text-blue-500">
-          <p className="text-sm font-bold underline">Kookjein's Discord Server</p>
-          <BiLinkExternal />
-        </div>
-      </a>
-      <a href="/">
-        <div className="flex space-x-2 items-center hover:text-blue-500">
-          <p className="text-sm font-bold underline">Download DEV Manual</p>
-          <BiLinkExternal />
-        </div>
-      </a>
-      <a href="/">
-        <div className="flex space-x-2 items-center hover:text-blue-500">
-          <p className="text-sm font-bold underline">Help Center</p>
-          <BiLinkExternal />
-        </div>
-      </a>
-    </div>
-  );
-
-  const MyProfileSummary = () => (
-    <div className="mt-4 w-full border rounded-xl flex-shrink-0 flex flex-col items-center p-6 bg-white shadow-lg">
-      <Link to={`/user/${userState.user.userId}`}>
-        <div className="flex flex-col items-center group">
-          <img
-            onError={({ currentTarget }) => {
-              currentTarget.onerror = null; // prevents looping
-              currentTarget.src = DefaultImage;
-            }}
-            src={DefaultImage}
-            alt=""
-            draggable={false}
-            className="w-16 h-16 bg-gray-100 rounded-full mb-4"
-          />
-          <p className="text-lg group-hover:text-green-600 group-hover:underline">Andrew Jang</p>
-          <p className="text-xs line-clamp-1 text-gray-600">안녕하세요 남산컴퍼니 / 국제인 대표 장동해입니다.</p>
-        </div>
-        <div className="w-full mt-6 text-sm text-green-700 font-bold">
-          <p>프로필을 완료하세요</p>
-          <div className="flex items-center space-x-3">
-            <div className="w-full h-1 rounded-full bg-gray-300">
-              <div className="w-1/2 h-full bg-green-700"></div>
-            </div>
-            <p className="text-xs font-normal text-gray-600">50%</p>
-          </div>
-        </div>
-      </Link>
-    </div>
-  );
-
-  return (
-    <div className="w-full h-full flex flex-col items-center overflow-x-hidden bg-gray-100">
-      <div
-        style={{ maxWidth: "1280px", scrollbarWidth: 0 }}
-        className="w-screen sm:w-full h-full flex-shrink-0 sm:justify-around px-4 pb-24"
-      >
-        <div className="w-full mt-8 flex">
-          <div className="w-full px-6 pb-12 space-y-4  py-4">
-            <Dashboard />
-          </div>
-          <div className="w-64 space-y-4 flex-shrink-0">
-            <MyProfileSummary />
-            <DeveloperHelp />
-            {selectedTab === "프로젝트 찾기" && <JobFilter />}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+        </div>);
 };
 
 export default MainDeveloper;
